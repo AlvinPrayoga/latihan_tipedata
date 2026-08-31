d71a11cc498ee30cdf21f7f0011b6ca6a83cebcf
Alvin prayoga <kedungkret@gmail.com>
2026-08-31 19:47:28 +0700

    versi2

---
+++ tipedata.dart
@@ -0,0 +1,13 @@
+void main() {
+String nama = 'Deni';
+ int umur = 100;
+ bool sudahmenikah = false;
+
+ var nama1 = 'Deni';
+ var angka1 = 100;
+
+print ('halo nama saya $nama. Umur saya $umur. Sudah menikah? $sudahmenikah');
+
+
+}
+
