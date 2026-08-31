2d405e241fe501768debba9bc08e931bc0d2356e
Alvin prayoga <kedungkret@gmail.com>
2026-08-31 20:38:10 +0700

    versi o

--- tipedata.dart
+++ tipedata.dart
@@ -1,12 +1,71 @@
 void main() {
-String nama = 'Deni';
- int umur = 100;
- bool sudahmenikah = false;
+ //String nama = 'Deni';
+ //int umur = 100;
+ //bool sudahmenikah = false;
 
- var nama1 = 'Deni';
- var angka1 = 100;
+ //var nama1 = 'Deni';
+ //var angka1 = 100;
 
-print ('halo nama saya $nama. Umur saya $umur. Sudah menikah? $sudahmenikah');
+//print ('halo nama saya $nama. Umur saya $umur. Sudah menikah? $sudahmenikah');
+//int angka = 100;
+//double angka2 = 20.5;
+
+//num hasil = 0;
+//notes num bisa menerima semua baik int maupun double, sedangkan kalo var hasil pakai int maka hasilnya error;
+
+
+//hasil = 0;
+//hasil = angka1 + angka2;
+//prin(hasil);
+
+//dynamic angka = 100;
+//dynamic angka2 = 200.5;
+//dynamic hasil = 0;
+//hasil = angka1+angka2;
+//print(hasil);
+//notes dynamic (dynamic adalah bisa isi variabel lain baik true or false ) juga bisa pakai var cuma var harus double supaya terhitung;
+
+//List (array) => kumpulan data yang memiliki data yang memiliki Index;
+//List <dynamic> namamhs = ['deni', 'sindy', 'miki', 100, true];
+//List  
+
+//List namamhs = [];
+//namamhs.add('Wilson1');
+//namamhs.add('Wilson2');
+//namamhs.add('Wilson3');
+//namamhs.add('Wilson4');
+
+//set => array yangb unik 
+//set angka = {1, 2, 3, 4, 1, 2, 3, 4};
+//print(angka.elementAt(2));
+
+//map => array yang memiliki key dan value 
+//Map datamhs = {'nama': 'ferry', 'umur': 30, 'kelas': 'A'};
+//print(object);
+
+//List<Map> datamhs = [
+//{'nama': 'Anton', 'umur': 30, 'kelas': 'A'},
+//{'nama': 'Deka', 'umur': 20, 'kelas': 'P'},
+//{'nama': 'ferry', 'umur': 25, 'kelas': 'Q'}
+//];
+//print('halo nama saya ${datamhs[1]['kelas']}');
+
+//final vs cont 
+//final String nama;
+//nama = 'ferry'; 
+//nama = 'deni'; 
+
+//const int angka = 100;
+//angka = 200;
+
+// const int angka = 100;
+// //angka = 200;
+
+// dart => null safety
+//String? nama;
+//int? angka; 
+//print(nama);
+
 
 
 }
